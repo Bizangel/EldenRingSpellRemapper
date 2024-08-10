e36400ce32eb8830c6a25129f7798aa57a93b4ed
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-08-10 16:47:05 -0500

    Fully integrated eslint automatically using vite-eslint-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), eslintPlugin()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
